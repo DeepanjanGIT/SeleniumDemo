55214a472ce154eff9c4d63fd0480573f47d55bf
DeepanjanGIT <deepban.13@gmail.com>
2022-09-01 09:23:02 +0530

    demo commit

---
+++ src/myproject/MyClass.java
@@ -0,0 +1,17 @@
+package myproject;
+
+
+
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		
+     
+      String message = "Helllo World"+"!!";
+      boolean a = message.endsWith("!!");
+      System.out.print(a);
+	}
+
+	
+}
